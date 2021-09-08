7e4e310f2b7f8dfa5d58669bc17bfc03fd4dc637
Anine Lome <aninelo@uio.no>
2021-09-08 16:01:28 +0200

    Started implementing methods push_back, push_front etc

--- problem1.py
+++ problem1.py
@@ -2,6 +2,21 @@ N = int(input())
 
 #Lage klasse
 
+def push_back(list, x):
+    list.back.next = newNode(x, prev = list.back)
+    list.back = list.back.next
+
+def push_front(list, x):
+    list.front.prev = newNode(x, next = list.front)
+    list.front = list.front.prev
+
+def push_middle(list, x):
+
+
+def get(list, i):
+
+
+
 for line in range(N):
     function, arg = input().split()
     getattr(function)(?, int(arg))
